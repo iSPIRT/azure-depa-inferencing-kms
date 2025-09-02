75600401c8e48efc920a1ca0874bc2ee173a35c9
Kapil Vaswani <kapil.vaswani@gmail.com>
2025-09-02 08:33:36 +0000

    new workflows

--- scripts/kms/jwt_issuer_trust.sh
+++ scripts/kms/jwt_issuer_trust.sh
@@ -108,7 +108,7 @@ jwt-issuer-get-policy-from-token() {
     decode_jwt "$1" | jq -r '{
             iss,
             sub,
-            idtyp,
+            idtyp: "app",
             oid
         } | with_entries(select(.value != null))'
 }
