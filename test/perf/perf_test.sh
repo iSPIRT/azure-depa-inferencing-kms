7a79bdc9982a75de4145f2ea7e62ed7fe314637c
Kapil Vaswani <kapil.vaswani@gmail.com>
2025-12-02 06:01:30 +0000

    delete files

--- test/perf/perf_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export KMS_URL=https://depa-inferencing-kms.centralindia.cloudapp.azure.com
-
-wrk --connections 100 --threads 100 --duration 30s --latency ${KMS_URL}/app/listpubkeys 
